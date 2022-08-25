82a72f8f9a3358597d78548caa8ec33787a0bcc7
Rajdwipbd <rajdwipmajumder271@gmail.com>
2022-08-25 21:27:49 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,15 @@
+var mobile = { name: 'Lenovo', Model: "xpd2", Price: '29990', Prosseror: 'Amd' };
+console.log(mobile);
+// kono properties ke result hosebe dekhte >>>>>
+
+console.log(mobile.name);
+console.log(mobile.Price);
+
+
+
+
+// next / 
+const Maruf = { ROll: 01, Age: 20, address: "Khulna Sador", };
+console.log(Maruf);
+
+console.log(Maruf.address)
